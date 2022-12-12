5cf73e2247d05ef5d9e66673f086a76922fff975
NiharGupte <nihar71298@gmail.com>
2022-12-12 23:24:34 +0530

    denoising

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# import os
-#
-# model_list = ["openai/whisper-tiny"]
-# dir_list = ["tiny"]
-#
-# for m, d in zip(model_list, dir_list):
-#     cmd = "python whisper_train.py --model_name " + m + " --model_dir " + d
-#     os.system(cmd)
 import os
 
 model_list = ["openai/whisper-tiny", "openai/whisper-base", "openai/whisper-medium"]
@@ -16,7 +8,6 @@ for m, d in zip(model_list, dir_list):
     os.system(cmd)
     cmd = "python whisper_train.py --model_name " + m + " --model_dir " + d
     os.system(cmd)
-    cmd = "python whisper_eval.py --model_name " + m + " --finetuned_model_name " + d
+    cmd = "python whisper_eval.py --model_name " + m + " --finetuned_model_name " + d + " --denoising 1"
     os.system(cmd)
-
     os.system("rm -r " + m + "_test.hf")
--- whisper_eval.py
+++ whisper_eval.py
@@ -13,40 +13,52 @@ from transformers import WhisperForConditionalGeneration
 from transformers import Seq2SeqTrainingArguments
 from transformers import Seq2SeqTrainer
 import os
-
+import scipy.wavfile.io as swi
 args = argparse.ArgumentParser()
 
 args.add_argument("--model_name", type=str, required=True)
 args.add_argument("--finetuned_model_name", type=str, required=True)
+args.add_argument("--denoising", type=bool, required=True)
 
 a = args.parse_args()
 
 model_name = a.model_name
 finetuned_model_name = a.finetuned_model_name
+denoise = a.denoising
 
 feature_extractor = WhisperFeatureExtractor.from_pretrained(model_name)
 tokenizer = WhisperTokenizer.from_pretrained(model_name, language="english", task="transcribe")
 processor = WhisperProcessor.from_pretrained(model_name, language="english", task="transcribe")
 model = WhisperForConditionalGeneration.from_pretrained(finetuned_model_name)
-#
-# def prepare_dataset(batch):
-#     # load and resample audio data from 48 to 16kHz
-#     audio = batch["audio"]
-#
-#     # compute log-Mel input features from input audio array
-#     batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
-#
-#     # encode target text to label ids
-#     batch["labels"] = tokenizer(batch["text"]).input_ids
-#     return batch
-#
-#
-# common_voice = load_dataset("pokameswaran/ami-6h")
-# common_voice = common_voice.remove_columns(["file", "length", "segment_id", "segment_start_time", "segment_end_time"])
-# common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
-# common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["train"], num_proc=2)
-data_path = model_name + "_test.hf"
-common_voice = datasets.load_from_disk(data_path)
+
+if denoise:
+    print("Applying denoising ...")
+    def prepare_dataset(batch):
+        # load and resample audio data from 48 to 16kHz
+        audio = batch["audio"]
+        swi.write("temp.wav", audio["array"], rate=16000)
+
+        est_sources = model.separate_file(path="test.wav")
+        arr = torchaudio.functional.resample(est_sources[:, :, 0].detach().cpu(), orig_freq=8000, new_freq=16000)
+
+        # compute log-Mel input features from input audio array
+        batch["input_features"] = feature_extractor(arr, sampling_rate=audio["sampling_rate"]).input_features[0]
+
+        # encode target text to label ids
+        batch["labels"] = tokenizer(batch["text"]).input_ids
+        return batch
+
+
+    common_voice = load_dataset("pokameswaran/ami-6h")
+    common_voice = common_voice.remove_columns(["file", "length", "segment_id", "segment_start_time", "segment_end_time"])
+    common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
+    common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["train"], num_proc=2)
+
+else:
+    print("Loading data from disk ... ")
+    data_path = model_name + "_test.hf"
+    common_voice = datasets.load_from_disk(data_path)
+
 
 @dataclass
 class DataCollatorSpeechSeq2SeqWithPadding:
