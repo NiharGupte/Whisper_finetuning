e178e1764533fe281db39a64383bb4ad883cf44c
NiharGupte <nihar71298@gmail.com>
2022-12-13 13:46:36 +0530

    New branch

--- data_preprocess.py
+++ data_preprocess.py
@@ -4,7 +4,7 @@ from transformers import WhisperFeatureExtractor
 from transformers import WhisperTokenizer
 from transformers import WhisperProcessor
 from datasets import Audio
-import torch
+import torchaudio
 import evaluate
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
@@ -12,32 +12,56 @@ from transformers import WhisperForConditionalGeneration
 from transformers import Seq2SeqTrainingArguments
 from transformers import Seq2SeqTrainer
 import os
+import scipy.io.wavfile as swi
+from speechbrain.pretrained import SepformerSeparation as separator
 
 args = argparse.ArgumentParser()
 
 args.add_argument("--model_name", type=str, required=True)
+args.add_argument("--denoise", type=int, required=True)
 
 a = args.parse_args()
 
 model_name = a.model_name
-
+denoise = a.denoise
 feature_extractor = WhisperFeatureExtractor.from_pretrained(model_name)
 tokenizer = WhisperTokenizer.from_pretrained(model_name, language="english", task="transcribe")
 processor = WhisperProcessor.from_pretrained(model_name, language="english", task="transcribe")
 model = WhisperForConditionalGeneration.from_pretrained(model_name)
+denoise_model = separator.from_hparams(source="speechbrain/sepformer-wham-enhancement",
+                                       savedir='pretrained_models/sepformer-wham-enhancement')
 
+if denoise==0:
+    def prepare_dataset(batch):
+        # load and resample audio data from 48 to 16kHz
+        audio = batch["audio"]
 
-def prepare_dataset(batch):
-    # load and resample audio data from 48 to 16kHz
-    audio = batch["audio"]
+        # compute log-Mel input features from input audio array
+        batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
 
-    # compute log-Mel input features from input audio array
-    batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+        # encode target text to label ids
+        batch["labels"] = tokenizer(batch["text"]).input_ids
+        return batch
+else:
 
-    # encode target text to label ids
-    batch["labels"] = tokenizer(batch["text"]).input_ids
-    return batch
+    def prepare_dataset(batch):
+        # load and resample audio data from 48 to 16kHz
+        audio = batch["audio"]
+        # print("first : ",audio["array"].shape)
+        swi.write("temp.wav", data=audio["array"], rate=16000)
 
+        est_sources = denoise_model.separate_file(path="temp.wav")
+        audio["array"] = torchaudio.functional.resample(est_sources[:, :, 0].detach().cpu(), orig_freq=8000,
+                                                        new_freq=16000).reshape((-1,))
+        # print("Second : ",audio["array"].shape)
+        # compute log-Mel input features from input audio array
+        batch["input_features"] = \
+        feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+
+        # encode target text to label ids
+        batch["labels"] = tokenizer(batch["text"]).input_ids
+        # print(batch, batch.keys())
+        return batch
 #
 common_voice = load_dataset("pokameswaran/ami-6h")
 common_voice = common_voice.remove_columns(["file", "length", "segment_id", "segment_start_time", "segment_end_time"])
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
-model_list = ["openai/whisper-tiny", "openai/whisper-base", "openai/whisper-medium"]
-dir_list = ["tiny", "base", "medium"]
+model_list = ["openai/whisper-medium"]
+dir_list = ["medium"]
 
 for m, d in zip(model_list, dir_list):
-    cmd = "python data_preprocess.py --model_name " + m
+    cmd = "python data_preprocess.py --model_name " + m + " --denoise 1"
     os.system(cmd)
     cmd = "python whisper_train.py --model_name " + m + " --model_dir " + d
     os.system(cmd)
-    cmd = "python whisper_eval.py --model_name " + m + " --finetuned_model_name " + d + " --denoising 1"
+    cmd = "python whisper_eval.py --model_name " + m + " --finetuned_model_name " + d + " --denoising 0"
     os.system(cmd)
     os.system("rm -r " + m + "_test.hf") #
